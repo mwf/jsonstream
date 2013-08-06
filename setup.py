71682d882aaa0e8fd77749174743ef4070be675c
Ivan Korolev <ikorolev@xyzrd.com>
2013-08-06 17:10:00 +0400

    Setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    author = 'Nigel Small',
+    author_email = 'nigel@nigelsmall.net',
+    description = 'Decodes JSON from a data stream',
+    download_url = 'http://github.com/mwf/jsonstream',
+    license = 'Apache License, Version 2.0',
+    name = 'jsonstream',
+    packages = ['jsonstream'],
+    version = '0.5.2'
+)
